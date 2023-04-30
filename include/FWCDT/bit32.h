3418bd77161a0b158cdceb4288f2c315bd2ab5ef
I.F.F. dos Santos <ismaellxd@gmail.com>
2023-04-30 16:38:08 -0300

    Updated the copy of the licence in some files.

--- include/FWCDT/bit32.h
+++ include/FWCDT/bit32.h
@@ -1,3 +1,32 @@
+/* *****************************************************************************
+   This file try to implement a type that storages 32 bit of memory and their
+   bitwise operators.
+
+   Last modified: April 30, 2023
+   *****************************************************************************
+   E-mail: ismaellxd@gmail.com
+   Site: https://ismaeldamiao.github.io/
+   *****************************************************************************
+   Copyright (c) 2023 I.F.F. dos SANTOS
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy 
+   of this software and associated documentation files (the “Software”), to 
+   deal in the Software without restriction, including without limitation the 
+   rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+   sell copies of the Software, and to permit persons to whom the Software is 
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in 
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+   IN THE SOFTWARE.
+***************************************************************************** */
 #ifndef BIT32_H
 #define BIT32_H 1
 
